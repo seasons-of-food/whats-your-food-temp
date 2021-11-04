196b8510caca50e8646e297e927a89a9d458fae6
Josshy <josshy92@hotmail.com>
2021-11-03 19:07:28 -0500

    made variables to connect to HTML in JS

---
+++ script.js
@@ -0,0 +1,4 @@
+var inputField = document.querySelector("#city")
+var submitButton = document.querySelector("#submit")
+var weatherBox = document.querySelector("#weatherbox")
+var recipesBox = document.querySelector("#recipes")
